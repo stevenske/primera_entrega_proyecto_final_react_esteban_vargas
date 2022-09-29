c63d5452712b436bf3d7ae5a766c8f783751cc5c
esteban vargas <esteban.var@outlook.com>
2022-09-28 21:34:21 -0300

    change style of cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -9,7 +9,7 @@ const Cart = () => {
     const totalProducts = cart.reduce((acc,prod)=>acc + prod.price * prod.quantity,0)
     return (
         <div className='row itemContainer gap-3 border border-dark rounded'>
-            <h1 className='text-center border-bottom border-dark py-3'>Cart</h1>
+            <h1 className='text-center border-bottom border-dark py-3 text-light bg-dark'>Cart</h1>
             {cart.length === 0 ?(
                 <>
                 <h2>the cart is empty :c</h2>
@@ -19,17 +19,17 @@ const Cart = () => {
             ):(
                 <>
                 {cart.map((product) => (
-                <div id={`${product.id}`} className="col-6 flex-row card w-50 card_size">
+                <div id={`${product.id}`} className="col-6 flex-row card w-50 card_size borderMix">
                 <img src={`${product.image}`} className="zoom" alt={product.title}/>
                     <div className="d-flex justify-content-center card-body">
                         <p className="card-title fs-3 fw-bold text-center">{product.title}</p>
                         <p className="card-title fs-3 fw-bold text-center">Quantity: {product.quantity}</p>
-                        <p className="card-title fs-3 fw-bold text-center">Price: {product.price * product.quantity}</p>
+                        <p className="card-title fs-3 fw-bold text-center rounded p-2 bg-dark text-light mx-auto ">Price: ${product.price * product.quantity}</p>
                     </div>
-                        <button className='btnRemove fs-3 px-5 py-1 align-self-center btn' onClick={() => removeProduct(product.id)}>X</button>
+                        <button className='btnRemove fs-3 px-5 align-self-center btn border border-dark' onClick={() => removeProduct(product.id)}>X</button>
             </div>
             ))}
-            <h6 className="d-flex justify-content-end fs-3 border-top py-3 border-dark text-center"><strong>TOTAL: ${totalProducts}</strong></h6>
+            <p className="total d-flex justify-content-end fs-3 border-top py-3 border-dark text-center text-light bg-dark"><strong>TOTAL: ${totalProducts}</strong></p>
             </>
             )}
         </div>
