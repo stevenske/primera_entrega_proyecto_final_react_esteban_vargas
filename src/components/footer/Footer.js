d0ad93a1b8500552ae3af6b6d243141b2bc0ab83
esteban vargas <esteban.var@outlook.com>
2022-10-19 22:08:11 -0300

    fix className on footer file

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,27 +6,27 @@ import instagram from '../assets/images/icon_instagram.png'
 import '../footer/Footer.css'
 const Footer = () => {
     return (
-        <footer class="py-3 border-top navDeco border-dark bg-ice">
-            <ul class="nav justify-content-center border-bottom border-dark pb-3 mb-3">
-                <li class="nav-item"><Link className='text-nav fs-4 px-3' to={'/'}>Home</Link></li>
-                <li class="nav-item"><Link className='text-nav fs-4 px-3' to={'/category/smartphone'}>Smartphones</Link></li>
-                <li class="nav-item"><Link className='text-nav fs-4 px-3' to={'/category/headphone'}>Headphones</Link></li>
+        <footer className="py-3 border-top navDeco border-dark bg-ice">
+            <ul className="nav justify-content-center border-bottom border-dark pb-3 mb-3">
+                <li className="nav-item"><Link className='text-nav fs-4 px-3' to={'/'}>Home</Link></li>
+                <li className="nav-item"><Link className='text-nav fs-4 px-3' to={'/category/smartphone'}>Smartphones</Link></li>
+                <li className="nav-item"><Link className='text-nav fs-4 px-3' to={'/category/headphone'}>Headphones</Link></li>
             </ul>
             <p class="text-center text-muted">© 2022 SmartFound, INC</p>
             <div class="container d-flex justify-content-around">
                 <div>
                     <a href="https://www.facebook.com" target="_blank">
-                        <img class="zoom" src={facebook} alt="logo of facebook"/>
+                        <img className="zoom" src={facebook} alt="logo of facebook"/>
                     </a>
                 </div>
                 <div>
                     <a href="https://www.instagram.com" target="_blank">
-                        <img class="zoom" src={instagram} alt="logo of instagram"/>
+                        <img className="zoom" src={instagram} alt="logo of instagram"/>
                     </a>
                 </div>
                 <div>
                     <a href="https://twitter.com" target="_blank">
-                        <img class="zoom" src={twitter} alt="logo of twitter"/>
+                        <img className="zoom" src={twitter} alt="logo of twitter"/>
                     </a>
                 </div>
             </div>
